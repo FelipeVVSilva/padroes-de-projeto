28a6f85a7281a5122f42b53542b0726a84dbec5e
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-11-15 11:35:39 -0300

    Factory Method - criando uma fábrica para cada objeto

--- creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -1,49 +1,23 @@
 package br.com.cod3r.factory.apple;
 
+import br.com.cod3r.factory.apple.factory.IPhone11ProFactory;
+import br.com.cod3r.factory.apple.factory.IPhoneFactory;
+import br.com.cod3r.factory.apple.factory.IPhoneXFactory;
 import br.com.cod3r.factory.apple.model.IPhone;
-import br.com.cod3r.factory.apple.model.IPhone11;
-import br.com.cod3r.factory.apple.model.IPhone11Pro;
-import br.com.cod3r.factory.apple.model.IPhoneX;
-import br.com.cod3r.factory.apple.model.IPhoneXSMax;
 
 public class Client {
-	
-	private IPhone orderIPhone(String model, String level){
-		IPhone device = null;
-
-		if(model.equals("X")) {
-			if(level.equals("standard")) {
-				device = new IPhoneX();
-			} else if(level.equals("highEnd")) {
-				device = new IPhoneXSMax();
-			}
-		} else if(model.equals("11")) {
-			if(level.equals("standard")) {
-				device = new IPhone11();
-			} else if(level.equals("highEnd")) {
-				device = new IPhone11Pro();
-			}
-		}
-
-		device.getHardware();
-		device.assemble();
-		device.certificates();
-		device.pack();
-
-		return device;
-	}
-
 
 	public static void main(String[] args) {
 
-		Client cliente = new Client();
+		IPhoneFactory iphoneXFactory = new IPhoneXFactory();
+		IPhoneFactory iphone11ProFactory = new IPhone11ProFactory();
 		
 		System.out.println("### Ordering an iPhone X");
-		IPhone iphone1 = cliente.orderIPhone("X", "standard");
+		IPhone iphone1 = iphoneXFactory.orderIphone();
 		System.out.println(iphone1);
 		
 		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
-		IPhone iphone2 = cliente.orderIPhone("11", "highEnd");
+		IPhone iphone2 = iphone11ProFactory.orderIphone();
 		System.out.println(iphone2);
 	}
 
